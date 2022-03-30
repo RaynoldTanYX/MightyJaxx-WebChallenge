e3a673d75766532e806458f1596b65baf55f12cb
Raynold Tan <raynoldtanyx@gmail.com>
2022-03-30 21:09:41 +0800

    chore: disable redux logging

--- admin-dashboard/src/store.js
+++ admin-dashboard/src/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 const middleware = [thunk];
 
 if (process.env.NODE_ENV === "development") {
-  middleware.push(logger);
+  // middleware.push(logger);
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
